2bc0d14e2264fa9d1336ff7ff0425c333120710e
adelovelace <anmero@fiec.espol.edu.ec>
2022-08-07 17:53:05 -0500

    changes on the decision tree structure

--- src/main/java/StructureData/DecisionTree.java
+++ src/main/java/StructureData/DecisionTree.java
@@ -1,60 +1,15 @@
 package StructureData;
-public class DecisionTree {
-    public class Node {
-        private String question;
-        private String answer;
-        private DecisionTree yes;
-        private DecisionTree no;
-        public Node(String question) {
-            this.question = question;
-            answer =null;
-        }
-        public void addAnswer(String answer) {
-            this.answer = answer;
-        }
-        public void addYes(DecisionTree yes) {
-            this.yes = yes;
-        }
-        public void addNo(DecisionTree no) {
-            this.no = no;
-        }
-        public boolean hadAnswer() {
-            return answer != null;
-        }
-        public String getAnswer() {
-            return answer;
-        }
-
-        public String getQuestion() {
-            return question;
-        }
-        public DecisionTree getYes() {
-            return yes;
-        }
-        public DecisionTree getNo() {
-            return no;
-        }
+public class DecisionTree<E> {
+    public class Node<E> {
+        Node<E> left;
+        Node<E> right;
+        E data;
     }
     public Node root;
 
-    public DecisionTree(String question) {
-        root = new Node(question);
+    public DecisionTree(E data) {
+        root.data = data;
+        root.left = root.right = null;
     }
 
-    public int heightTree(Node node) {
-
-        if (node == null)
-            return -1;
-        else
-        {
-            int lheight = heightTree(node.yes.root);
-            int rheight = heightTree(node.no.root);
-
-            if (lheight > rheight)
-                return (lheight + 1);
-            else
-                return (rheight + 1);
-        }
-    }
-    
 }

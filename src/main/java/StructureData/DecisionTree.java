3c12da5a9b047ad515b25a3e2dc66048c250aa90
adelovelace <anmero@fiec.espol.edu.ec>
2022-08-08 15:06:06 -0500

    misc changes structure

--- src/main/java/StructureData/DecisionTree.java
+++ src/main/java/StructureData/DecisionTree.java
@@ -4,12 +4,47 @@ public class DecisionTree<E> {
         Node<E> left;
         Node<E> right;
         E data;
+
+        Node(E data){
+            this.data = data;
+            this.left = this.right = null;
+        }
+
+        public Node<E> getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node<E> left) {
+            this.left = left;
+        }
+
+        public Node<E> getRight() {
+            return right;
+        }
+
+        public void setRight(Node<E> right) {
+            this.right = right;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public void setData(E data) {
+            this.data = data;
+        }
     }
     public Node root;
 
     public DecisionTree(E data) {
         root.data = data;
-        root.left = root.right = null;
     }
 
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
 }
